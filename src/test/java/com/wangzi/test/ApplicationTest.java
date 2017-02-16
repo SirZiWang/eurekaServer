6b63dc49031815c35716d4eb5c89efbe5b4ebffe
wangjun <928639375@qq.com>
2017-02-16 10:50:42 +0800

    eureka服务

---
+++ src/test/java/com/wangzi/test/ApplicationTest.java
@@ -0,0 +1,20 @@
+package com.wangzi.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class ApplicationTest {
+
+	@RequestMapping(value="/")
+	String home(){
+		return "Hello Word";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationTest.class, args);
+	}
+}
